7d5b7a4716fb9459e6eec51442ddd04d3be662b8
Jimmy Blundell <jblundell123@gmail.com>
2023-02-24 16:53:21 -0600

    work on form, still not lined up but at least centered

--- frontend/src/Pages/OrderForm.tsx
+++ frontend/src/Pages/OrderForm.tsx
@@ -2,93 +2,84 @@ import React, { useState } from 'react';
 import { Trade } from './OrderBook';
 
 type OrderFormProps = {
-  onAddTrade: (trade: Trade) => void;
+    onAddTrade: (trade: Trade) => void;
 };
 
 const OrderForm = ({ onAddTrade }: OrderFormProps) => {
-  const [side, setSide] = useState('buy');
-  const [amount, setAmount] = useState('');
-  const [amountType, setAmountType] = useState('shares');
-  const [price, setPrice] = useState('');
-  const [gtc, setGtc] = useState(false);
-  const [expiration, setExpiration] = useState('');
+    const [side, setSide] = useState('buy');
+    const [amount, setAmount] = useState('');
+    const [amountType, setAmountType] = useState('shares');
+    const [price, setPrice] = useState('');
+    const [gtc, setGtc] = useState(false);
+    const [expiration, setExpiration] = useState('');
 
-  function handleSubmit(event: any) {
-    event.preventDefault();
+    function handleSubmit(event: any) {
+        event.preventDefault();
 
-    const trade = {
-      side,
-      amount: amountType === 'shares' ? parseInt(amount) : parseFloat(amount),
-      price: parseFloat(price),
-      gtc,
-      expiration: gtc ? null : expiration
-    };
+        const trade = {
+            side,
+            amount: amountType === 'shares' ? parseInt(amount) : parseFloat(amount),
+            price: parseFloat(price),
+            gtc,
+            expiration: gtc ? null : expiration
+        };
 
-    onAddTrade(trade);
+        onAddTrade(trade);
 
-    // Reset form fields
-    setSide('buy');
-    setAmount('');
-    setAmountType('shares');
-    setPrice('');
-    setGtc(false);
-    setExpiration('');
-  }
+        // Reset form fields
+        setSide('buy');
+        setAmount('');
+        setAmountType('shares');
+        setPrice('');
+        setGtc(false);
+        setExpiration('');
+    }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Side:
-        <select value={side} onChange={(event) => setSide(event.target.value)}>
-          <option value="buy">Buy</option>
-          <option value="sell">Sell</option>
-        </select>
-      </label>
-      <label>
-        Amount:
-        <input
-          type="number"
-          value={amount}
-          onChange={(event) => setAmount(event.target.value)}
-        />
-        <select
-          value={amountType}
-          onChange={(event) => setAmountType(event.target.value)}
-        >
-          <option value="shares">Shares</option>
-          <option value="dollars">$</option>
-        </select>
-      </label>
-      <label>
-        Price:
-        <input
-          type="number"
-          step="0.01"
-          value={price}
-          onChange={(event) => setPrice(event.target.value)}
-        />
-      </label>
-      <label>
-        GTC:
-        <input
-          type="checkbox"
-          checked={gtc}
-          onChange={(event) => setGtc(event.target.checked)}
-        />
-      </label>
-      {!gtc && (
-        <label>
-          Expiration:
-          <input
-            type="date"
-            value={expiration}
-            onChange={(event) => setExpiration(event.target.value)}
-          />
-        </label>
-      )}
-      <button type="submit">Submit</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit} style={{ width: '50%', margin: '0 auto', textAlign: 'center' }}>
+            <label style={{ float: "left", display: 'block', margin: '1em 0' }}>
+                Side:
+                <select value={side} onChange={(e) => setSide(e.target.value)}>
+                    <option value="buy">Buy</option>
+                    <option value="sell">Sell</option>
+                </select>
+            </label>
+            <label style={{ display: 'block', margin: '1em 0' }}>
+                Amount:
+                <input
+                    type="number"
+                    value={amount}
+                    onChange={(e) => setAmount((e.target.value))}
+                    style={{ width: '100%' }}
+                />
+            </label>
+            <label style={{ display: 'block', margin: '1em 0' }}>
+                Price:
+                <input
+                    type="number"
+                    value={price}
+                    onChange={(e) => setPrice((e.target.value))}
+                    style={{ width: '100%' }}
+                />
+            </label>
+            <label style={{ float: "left", display: 'block', margin: '1em 0' }}>
+                Good 'til Canceled:
+                <input type="checkbox" checked={gtc} onChange={(e) => setGtc(e.target.checked)} />
+            </label>
+            {!gtc && (
+                <label style={{ display: 'block', margin: '1em 0' }}>
+                    Expiration:
+                    <input
+                        type="date"
+                        value={expiration}
+                        onChange={(e) => setExpiration(e.target.value)}
+                        style={{ width: '100%' }}
+                    />
+                </label>
+            )}
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 
 export default OrderForm;
